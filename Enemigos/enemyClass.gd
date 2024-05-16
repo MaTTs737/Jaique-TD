9615acdbcb5f17e2228294a8035b10e3501a55d9
unknown <m.solanilla37@gmail.com>
2024-05-16 20:59:19 +0200

    Items clase enemigo y clase torre

---
+++ Enemigos/enemyClass.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var healthPoints: int
+var type: String
+var speed : int
+var damage : int
+var reward : int
+var specialCondition = false
+
+
+func getSpecialCondition():
+	specialCondition = true
+	$specialCondition.start()
+	
+func die():
+	queue_free()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if healthPoints <= 0:
+		die()
+
+
+func _on_special_condition_timeout():
+	specialCondition = false
---
+++ Torres/torreClass.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var damage : int 
+var type : String
+var cost : int
+var waitingTime : int
+var range : int
+var target : Object
+var priorTargegt : Object
+	
+func identifyEnemy(): #elige entre los enemigos que esten en rango uno para usar de target
+	pass
+
+func shoot():
+	pass
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
