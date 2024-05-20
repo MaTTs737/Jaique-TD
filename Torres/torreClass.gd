e866c1e5d3b6e70928da40dde0227f27a8f89863
unknown <m.solanilla37@gmail.com>
2024-05-20 20:53:20 +0200

    avances en torreClass

--- Torres/torreClass.gd
+++ Torres/torreClass.gd
@@ -6,22 +6,30 @@ var cost : int
 var waitingTime : int = 3
 var range : int
 var target : Object
-
-var enemyInCall = Callable($areaVision,"_on_DetectionArea_body_entered")
-var enemyOutCall = Callable($areaVision,"_on_DetectionArea_body_exited")
-var priorTargegt : Object
-
-func identifyEnemy(): #elige entre los enemigos que esten en rango uno para usar de target
-	var enemy_list=[]
+var enemies_in_range = []
+var enemyIn = Callable(self,"_on_DetectionArea_body_entered")
+var enemyOut = Callable(self,"_on_DetectionArea_body_exited")
+var projectil = preload("res://Torres/Projectiles/projectilClass.tscn")
 
 func shoot():
-	pass
-	
+	target = enemies_in_range[0]
+	var disparo = projectil.new
+	disparo.target = target
+	disparo.type = type	
 
-# Called when the node enters the scene tree for the first time.
+	# Called when the node enters the scene tree for the first time.
 func _ready():
 	$shootTimer.wait_time=waitingTime
+	$DetectionArea.connect("body_entered", enemyIn)
+	$DetectionArea.connect("body_exited", enemyOut)
 
+func _on_DetectionArea_body_entered(body):
+	if body.is_in_group("enemies"):
+		enemies_in_range.append(body)
+
+func _on_DetectionArea_body_exited(body):
+	if body.is_in_group("enemies"):
+		enemies_in_range.erase(body)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
