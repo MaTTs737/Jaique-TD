af6d7a4d29338299e1dd6d544abbc094336b4cea
¨MaTTs737¨ <m.solanilla37@gmail.com>
2024-08-13 13:13:39 +0200

    cutscene

--- Sistema/Cutscenes/presentation.gd
+++ Sistema/Cutscenes/presentation.gd
@@ -8,8 +8,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("pausa"):
+		get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_finished():
 	get_tree().change_scene_to_file("res://main.tscn")
+
