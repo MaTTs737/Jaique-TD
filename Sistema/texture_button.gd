0f9558d41e05bc54dd9f61fc2992ea27ab1eedb1
¨MaTTs737¨ <m.solanilla37@gmail.com>
2024-10-30 21:05:55 +0100

    pantalla tutorial

---
+++ Sistema/texture_button.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+#func _process(delta: float) -> void:
+#	if is_hovered():
+#		$TextureRect.visible = true
+#		$RichTextLabel.visible = true
+#	else:
+#		$TextureRect.visible = false	
+#		$RichTextLabel.visible = false 
+#		
